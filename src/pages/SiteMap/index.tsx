db04cd2b8c4b5acde88f8f1802f8fef087e731ce
Juan Carlos Sequeira Piedra <jcspiedra@gmail.com>
2022-06-29 18:36:46 -0600

    feat: add video

--- src/pages/SiteMap/index.tsx
+++ src/pages/SiteMap/index.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/iframe-has-title */
 import {
   IonButton,
   IonCol,
@@ -5,42 +6,49 @@ import {
   IonGrid,
   IonPage,
   IonRow,
-} from '@ionic/react'
-import Header from '../../components/Header/Header'
+} from "@ionic/react";
+import Header from "../../components/Header/Header";
 
 const SiteMap: React.FC = () => {
   return (
     <IonPage>
-      <Header title='Mapa del sitio' />
+      <Header title="Mapa del sitio" />
       <IonContent>
         <IonGrid>
-          <IonRow className='ion-justify-content-center'>
-            <IonCol size='12' className='ion-text-center'>
-              <IonButton href='/page/home'>Inicio</IonButton>
+          <IonRow className="ion-justify-content-center">
+            <IonCol size="12" className="ion-text-center">
+              <IonButton href="/page/home">Inicio</IonButton>
             </IonCol>
           </IonRow>
-          <IonRow className='ion-justify-content-center'>
-            <IonCol size='6' className='ion-text-center'>
-              <IonButton href='/page/tourist-routes'>
+          <IonRow className="ion-justify-content-center">
+            <IonCol size="6" className="ion-text-center">
+              <IonButton href="/page/tourist-routes">
                 Zonas turísticas
               </IonButton>
             </IonCol>
-            <IonCol size='6' className='ion-text-center'>
-              <IonButton href='/page/site-map'>Mapa del sito</IonButton>
+            <IonCol size="6" className="ion-text-center">
+              <IonButton href="/page/site-map">Mapa del sito</IonButton>
             </IonCol>
           </IonRow>
-          <IonRow className='ion-justify-content-center'>
-            <IonCol size='6' className='ion-text-center'>
-              <IonButton href='/page/about-us'>Acerca de nosotros</IonButton>
+          <IonRow className="ion-justify-content-center">
+            <IonCol size="6" className="ion-text-center">
+              <IonButton href="/page/about-us">Acerca de nosotros</IonButton>
             </IonCol>
-            <IonCol size='6' className='ion-text-center'>
-              <IonButton href='/page/contact-us'>Contactanos</IonButton>
+            <IonCol size="6" className="ion-text-center">
+              <IonButton href="/page/contact-us">Contactanos</IonButton>
             </IonCol>
+          </IonRow>
+          <IonRow className="ion-justify-content-center" style={{marginTop: '1.5rem'}}>
+            <iframe
+              width="95%"
+              height="315"
+              src="https://www.youtube.com/embed/dVV0UpFJRNE"
+            ></iframe>
           </IonRow>
         </IonGrid>
       </IonContent>
     </IonPage>
-  )
-}
+  );
+};
 
-export default SiteMap
+export default SiteMap;
