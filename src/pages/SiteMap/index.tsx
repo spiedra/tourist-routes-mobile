2305666fff8d6271c6fbfb3c6a03068c325b3428
Fabricio MSI <carlos.agueroelizondo@ucr.ac.cr>
2022-06-29 18:27:17 -0600

    Feat/ Added the site map content

--- src/pages/SiteMap/index.tsx
+++ src/pages/SiteMap/index.tsx
@@ -1,12 +1,46 @@
-import { IonPage } from "@ionic/react";
-import Header from "../../components/Header/Header";
+import {
+  IonButton,
+  IonCol,
+  IonContent,
+  IonGrid,
+  IonPage,
+  IonRow,
+} from '@ionic/react'
+import Header from '../../components/Header/Header'
 
 const SiteMap: React.FC = () => {
   return (
     <IonPage>
-      <Header title="Mapa del sitio" />
+      <Header title='Mapa del sitio' />
+      <IonContent>
+        <IonGrid>
+          <IonRow className='ion-justify-content-center'>
+            <IonCol size='12' className='ion-text-center'>
+              <IonButton href='/page/home'>Inicio</IonButton>
+            </IonCol>
+          </IonRow>
+          <IonRow className='ion-justify-content-center'>
+            <IonCol size='6' className='ion-text-center'>
+              <IonButton href='/page/tourist-routes'>
+                Zonas turísticas
+              </IonButton>
+            </IonCol>
+            <IonCol size='6' className='ion-text-center'>
+              <IonButton href='/page/site-map'>Mapa del sito</IonButton>
+            </IonCol>
+          </IonRow>
+          <IonRow className='ion-justify-content-center'>
+            <IonCol size='6' className='ion-text-center'>
+              <IonButton href='/page/about-us'>Acerca de nosotros</IonButton>
+            </IonCol>
+            <IonCol size='6' className='ion-text-center'>
+              <IonButton href='/page/contact-us'>Contactanos</IonButton>
+            </IonCol>
+          </IonRow>
+        </IonGrid>
+      </IonContent>
     </IonPage>
-  );
-};
+  )
+}
 
-export default SiteMap;
+export default SiteMap
